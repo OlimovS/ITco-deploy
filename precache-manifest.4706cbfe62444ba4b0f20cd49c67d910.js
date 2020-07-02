fa4e059f9e005b2653c35e6bb8f5724a4d1f5808
Sulaymon Olimov <sulaymonlucky@gmail.com>
2020-07-02 15:50:10 +0500

    Updates

---
+++ precache-manifest.4706cbfe62444ba4b0f20cd49c67d910.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa31b016615dd7f3f32d1f37ba65e8e2",
+    "url": "/ITco/index.html"
+  },
+  {
+    "revision": "3c6eb7021360968ca0cc",
+    "url": "/ITco/static/css/main.e58222d1.chunk.css"
+  },
+  {
+    "revision": "0a5dcfb0ba6307fb71bc",
+    "url": "/ITco/static/js/2.20ac201f.chunk.js"
+  },
+  {
+    "revision": "14d3db2f0aae1bbe6b1122194ce299b3",
+    "url": "/ITco/static/js/2.20ac201f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6eb7021360968ca0cc",
+    "url": "/ITco/static/js/main.23869596.chunk.js"
+  },
+  {
+    "revision": "ac63a7c957446a193051",
+    "url": "/ITco/static/js/runtime-main.bde79339.js"
+  },
+  {
+    "revision": "bd1ca15730bbe2f50442bfc0c0cab08b",
+    "url": "/ITco/static/media/hand.bd1ca157.png"
+  },
+  {
+    "revision": "d49005b4c4fd51ec0d93f26a7534a2a2",
+    "url": "/ITco/static/media/hero.d49005b4.png"
+  },
+  {
+    "revision": "1cd8a44130158a1372e7c816bbd59ceb",
+    "url": "/ITco/static/media/keyboard.1cd8a441.png"
+  },
+  {
+    "revision": "73623b2503e00d04a69bbd20bf03d063",
+    "url": "/ITco/static/media/mac.73623b25.png"
+  },
+  {
+    "revision": "5f3877a442eb13404019bf9f29f41f16",
+    "url": "/ITco/static/media/pic1.5f3877a4.png"
+  },
+  {
+    "revision": "aaecb510ff720cba813a3b2b2cead34f",
+    "url": "/ITco/static/media/pic1.aaecb510.png"
+  },
+  {
+    "revision": "2a816dae37a9cbc3075de8f159dcb7d0",
+    "url": "/ITco/static/media/pic2.2a816dae.png"
+  },
+  {
+    "revision": "49b34dfdd00b09c9a2f1106f4202f8f9",
+    "url": "/ITco/static/media/pic2.49b34dfd.png"
+  },
+  {
+    "revision": "35d239315b36300a95355e7fe213d85c",
+    "url": "/ITco/static/media/pic3.35d23931.png"
+  },
+  {
+    "revision": "f4597d6ebfe8fa3bdc941e9daba8d3cd",
+    "url": "/ITco/static/media/pic3.f4597d6e.png"
+  },
+  {
+    "revision": "e5943123009f5aad0ad80afef521a07f",
+    "url": "/ITco/static/media/pic4.e5943123.png"
+  },
+  {
+    "revision": "b4e63e3f26c86f8e0bd12b2c05929301",
+    "url": "/ITco/static/media/pic5.b4e63e3f.png"
+  },
+  {
+    "revision": "c1e6b068b499b818d317863afb959d1b",
+    "url": "/ITco/static/media/pic6.c1e6b068.png"
+  },
+  {
+    "revision": "5a05cf508fb8485d42197e11fc9e2d15",
+    "url": "/ITco/static/media/play.5a05cf50.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ITco/precache-manifest.4706cbfe62444ba4b0f20cd49c67d910.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ITco/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
